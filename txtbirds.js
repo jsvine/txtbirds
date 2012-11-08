b9aca8ad44b69ea2d77e73a5765b18786ad3ae8c
Jeremy Singer-Vine <jsvine@gmail.com>
2012-11-07 23:57:59 -0500

    Initial code commit.

---
+++ txtbirds.js
@@ -0,0 +1,55 @@
+(function () {
+	var root = this;
+	var gyre = root.document.getElementById("gyre");
+
+	var Falcon = function () {
+		this.flaps = [ '------', '/‾\\/‾\\', '‾‾\\/‾‾' ];
+		this.flap_i = 0;
+		this.flap_rate = 230;
+		this.veer_rate = 500;
+		this.veer_coefficient_vertical = 75;
+		this.veer_coefficient_horizontal = 50;
+		this.lifespan = 15 * 1000;
+		this.timers = [];
+	};
+
+	Falcon.prototype = {
+		hatch: function () {
+			var _this = this;
+			this.el = root.document.createElement("span");
+			this.el.className = "falcon";
+			[ "top", "left" ].forEach(function (s) { 
+				_this.el.style[s] = (50 + (root.Math.random() < 0.5 ? -1 : 1) * root.Math.random() * 80) + "%"; 
+			});
+			gyre.appendChild(this.el);
+			return this;
+		},
+		flap: function () { 
+			this.el.innerHTML = this.flaps[this.flap_i++ % this.flaps.length];
+		},
+		veer: function () {
+			this.el.style.marginTop = root.Math.random() * this.veer_coefficient_vertical / root.Math.log(this.flap_i + 1) + "px";
+			this.el.style.marginLeft = root.Math.random() * this.veer_coefficient_horizontal / root.Math.log(this.flap_i + 1) + "px";
+		},
+		fly: function () {
+			var _this = this;
+			root.setTimeout(function () { _this.el.className += " flying"; }, 10);
+			this.timers.push(root.setInterval(function () { _this.flap.call(_this); }, this.flap_rate));
+			this.timers.push(root.setInterval(function () { _this.veer.call(_this); }, this.veer_rate));
+			root.setTimeout(function () { _this.vanish.call(_this); }, this.lifespan);
+			return this;
+		},
+		vanish: function () {
+			this.timers.forEach(function (t) { root.clearInterval(t); });
+			gyre.removeChild(this.el);
+			return this;
+		}
+	};
+
+	var launch = function () {
+		return (new Falcon()).hatch().fly();
+	};
+
+	root.setInterval(launch, 730);
+
+}).call(this);
